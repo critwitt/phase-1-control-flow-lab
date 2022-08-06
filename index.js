c3bda831fee0da9c99ab7edb905e9b6ee7f91f52
Chris Witte <christopher.t.witte@gmail.com>
2022-08-06 19:39:01 -0400

    commit tree 6fa3227b8220f2c4ac1b3673423f7056b81d1eee

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    const msg = "This one is on me!";
+    return msg
+  }
+  else if (distance < 2000) {
+    const msg = "That will be twenty bucks.";
+    return msg
+  }
+  else if (distance < 2500) {
+    const msg = "I will gladly take your thirty bucks.";
+    return msg
+  }
+  else {
+    const msg = "No can do.";
+    return msg
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    const msg = "Ok, sounds good.";
+    return msg
+  }
+  else {
+    const msg = "No go.";
+    return msg
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    const msg = "Thank you so much.";
+    return msg
+  }
+  else if (tip === "not as generous") {
+    const msg = "Thank you.";
+    return msg
+  }
+  else {
+    const msg = "Bye.";
+    return msg
+  }
 }
